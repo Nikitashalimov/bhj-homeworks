f76b21cd950dc26ead404133c79cfaceaf422b20
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2021-08-26 22:26:37 +0300

    Add files via upload

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,8 +1,29 @@
-let menuLinks = document.querySelectorAll('.menu__link');
-let menuSub = document.querySelectorAll('.menu_sub');
+
+const menuLinks = document.querySelectorAll('.menu__link');
+
+for (let i = 0; i < menuLinks.length; i++) {
+	menuLinks[i].addEventListener("click", openEvent);
+}
+
+function openEvent() {
+	this.nextElementSibling.onclick = function () {
+		return false;
+	}
+	if (this.nextElementSibling.className === 'menu menu_sub menu_active') {
+		this.nextElementSibling.classList.remove('menu_active');
+	} else {
+		this.nextElementSibling.classList.add('menu_active');
+	}
+}
+
+//это было началом первого варианта решения...безуспешный совсем...чего-то мне не хватило...
+
+
+const menuLinks = document.querySelectorAll('.menu__link');
+const menuSub = document.querySelectorAll('.menu');
 
 for (let menuLink of menuLinks) {
 	menuLink.onclick = function () {
-		menuSub.classList.add('menu_active');
+		this.nextElementSibling.classList.add('menu_active')
 	}
 }
