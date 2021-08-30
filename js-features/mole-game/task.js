464ebb5626ac1456f348c3bbc0780ce2cf963a79
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2021-08-30 18:29:24 +0300

    Add files via upload

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,27 +1,37 @@
 let holes = document.querySelectorAll('.hole');
-let dead = document.getElementById('dead').innerHTML;
-let lost = document.getElementById('lost').innerHTML;
+let dead = document.getElementById('dead').textContent;
+let lost = document.getElementById('lost').textContent;
 
 for (let i = 0; i < holes.length; i++) {
 	holes[i].addEventListener('click', clickHole);
 }
 
+function resetScore() {
+	dead = 0;
+	lost = 0;
+	document.getElementById('dead').textContent = 0;
+	document.getElementById('lost').textContent = 0;
+}
+
 function clickHole() {
-	if (this.classList.contains('hole hole_has-mole')) {
+	if (this.classList.contains('hole_has-mole')) {
 		console.log('Попал');
 		if (dead == '10') {
-			alert('Ура!!! Победа!!!')
+			alert('Ура!!! Победа!!!');
+			resetScore();
 		} else {
 			dead = Number(dead) + 1;
-			document.getElementById('dead').innerHTML = dead;
+			document.getElementById('dead').textContent = dead;
 		}
 	} else {
 		console.log('Мазила');
 		if (lost == '5') {
 			alert('Вы проиграли!!!');
+			resetScore();
 		} else {
 			lost = Number(lost) + 1;
-			document.getElementById('lost').innerHTML = lost;
+			document.getElementById('lost').textContent = lost;
 		}
 	}
 }
+
