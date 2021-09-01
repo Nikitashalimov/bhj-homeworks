662f0180c081abeaf6d225c79693503d65dcfa5c
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2021-09-01 21:08:15 +0300

    Add files via upload

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,29 +1,23 @@
-
-const menuLinks = document.querySelectorAll('.menu__link');
+const menuLinks = document.querySelectorAll('a[href=""]');
+const subMenuActives = document.querySelectorAll('.menu_sub');
 
 for (let i = 0; i < menuLinks.length; i++) {
 	menuLinks[i].addEventListener("click", openEvent);
 }
 
-function openEvent() {
-	this.nextElementSibling.onclick = function () {
-		return false;
-	}
-	if (this.nextElementSibling.className === 'menu menu_sub menu_active') {
-		this.nextElementSibling.classList.remove('menu_active');
-	} else {
-		this.nextElementSibling.classList.add('menu_active');
-	}
-}
-
-//это было началом первого варианта решения...безуспешный совсем...чего-то мне не хватило...
-
-
-const menuLinks = document.querySelectorAll('.menu__link');
-const menuSub = document.querySelectorAll('.menu');
-
-for (let menuLink of menuLinks) {
-	menuLink.onclick = function () {
-		this.nextElementSibling.classList.add('menu_active')
+function removeClass() {
+	for (subMenuActive of subMenuActives) {
+		if (this.subMenuActive.classList.contains('menu_active')) {
+			subMenuActive.classList.remove('menu_active');
+		} else {
+			break;
+		}
 	}
 }
+
+function openEvent(e) {
+	removeClass();
+	e.preventDefault();
+	this.nextElementSibling.classList.add('menu_active');
+}
+
