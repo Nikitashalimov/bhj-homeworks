c6fa7427287cc9f5908e755937d004aa0e50aef4
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2021-09-01 21:07:24 +0300

    Add files via upload

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,36 +1,54 @@
 const buttonPrev = document.querySelector('.slider__arrow_prev');
 const buttonNext = document.querySelector('.slider__arrow_next');
-const sliderItem = document.querySelectorAll('.slider__item');
+const sliderItems = document.querySelectorAll('.slider__item');
+const sliderDots = document.querySelectorAll('.slider__dot');
 
 buttonPrev.addEventListener("click", slidePrev);
 buttonNext.addEventListener("click", slideNext);
 
 let slideIndex = 1;
-showSlide(slideIndex);
+function currentIndex(index) {
+	showSlide(index);
+	showDot(index);
+}
 
-function slidePrev() {
-	showSlide(slideIndex -= 1);
-	console.log(slideIndex)
-};
+const showSlide = n => {
+	for (sliderItem of sliderItems) {
+		sliderItem.classList.remove('slider__item_active');
+	}
+	sliderItems[n].classList.add('slider__item_active');
+}
+
+const showDot = n => {
+	for (sliderDot of sliderDots) {
+		sliderDot.classList.remove('slider__dot_active');
+	}
+	sliderDots[n].classList.add('slider__dot_active');
+}
 
 function slideNext() {
-	showSlide(slideIndex += 1);
-	console.log(slideIndex)
+	if (slideIndex == sliderItems.length - 1) {
+		slideIndex = 0;
+		currentIndex(slideIndex);
+	} else {
+		slideIndex++;
+		currentIndex(slideIndex);
+	}
 };
 
-function slideCurrent(n) {
-	showSlide(slideIndex = n);
-}
+function slidePrev() {
+	if (slideIndex == 0) {
+		slideIndex = sliderItems.length - 1;
+		currentIndex(slideIndex);
+	} else {
+		slideIndex--;
+		currentIndex(slideIndex);
+	}
+};
 
-function showSlide(n) {
-	if (n > sliderItem.length) {
-		slideIndex = 1
-	}
-	if (n < 1) {
-		slideIndex = sliderItem.length
-	}
-	for (let i = 0; i < sliderItem.length; i++) {
-		sliderItem[i].classList.add("slider__item_active");
-	}
-	sliderItem[slideIndex - 1].classList.remove("slider__item_active")
-}
+sliderDots.forEach((item, indexDot) => {
+	item.addEventListener('click', () => {
+		slideIndex = indexDot;
+		currentIndex(slideIndex);
+	})
+})
