d7762aa416fe740806d68a2965132afac8d0135f
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2021-09-06 21:52:25 +0300

    Add files via upload

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,54 +1,35 @@
 const buttonPrev = document.querySelector('.slider__arrow_prev');
 const buttonNext = document.querySelector('.slider__arrow_next');
-const sliderItems = document.querySelectorAll('.slider__item');
+const sliderItems = Array.from(document.querySelectorAll('.slider__item'));
 const sliderDots = document.querySelectorAll('.slider__dot');
+let index = 0;
 
 buttonPrev.addEventListener("click", slidePrev);
 buttonNext.addEventListener("click", slideNext);
 
-let slideIndex = 1;
-function currentIndex(index) {
-	showSlide(index);
-	showDot(index);
+function currentIndex(slideActive, index) {
+	showSlide(slideActive, index);
+	showDot(slideActive, index);
 }
 
-const showSlide = n => {
-	for (sliderItem of sliderItems) {
-		sliderItem.classList.remove('slider__item_active');
-	}
-	sliderItems[n].classList.add('slider__item_active');
+const showSlide = (slideActive, index) => {
+	sliderItems[slideActive].classList.remove('slider__item_active');
+	sliderItems[index].classList.add('slider__item_active');
 }
 
-const showDot = n => {
-	for (sliderDot of sliderDots) {
-		sliderDot.classList.remove('slider__dot_active');
-	}
-	sliderDots[n].classList.add('slider__dot_active');
+const showDot = (slideActive, index) => {
+	sliderDots[slideActive].classList.remove('slider__dot_active');
+	sliderDots[index].classList.add('slider__dot_active');
 }
 
 function slideNext() {
-	if (slideIndex == sliderItems.length - 1) {
-		slideIndex = 0;
-		currentIndex(slideIndex);
-	} else {
-		slideIndex++;
-		currentIndex(slideIndex);
-	}
-};
+	let slideActive = sliderItems.findIndex(item => item.classList.contains('slider__item_active'));
+	(slideActive == sliderItems.length - 1) ? index = 0 : index++;
+	currentIndex(slideActive, index);
+}
 
 function slidePrev() {
-	if (slideIndex == 0) {
-		slideIndex = sliderItems.length - 1;
-		currentIndex(slideIndex);
-	} else {
-		slideIndex--;
-		currentIndex(slideIndex);
-	}
-};
-
-sliderDots.forEach((item, indexDot) => {
-	item.addEventListener('click', () => {
-		slideIndex = indexDot;
-		currentIndex(slideIndex);
-	})
-})
+	let slideActive = sliderItems.findIndex(item => item.classList.contains('slider__item_active'));
+	(slideActive == 0) ? index = sliderItems.length - 1 : index--;
+	currentIndex(slideActive, index);
+}
