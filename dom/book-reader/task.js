251751ebf89b89fee25bf3c0720e2850ff57f317
Никита Шалимов <78145635+Nikitashalimov@users.noreply.github.com>
2021-09-14 21:26:19 +0300

    Add files via upload

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -2,7 +2,7 @@ let sizeButtons = document.querySelectorAll('.font-size');
 let book = document.getElementById('book');
 let textColors = (document.querySelector('.book__control_color')).querySelectorAll('.color');
 let bgColors = (document.querySelector('.book__control_background')).querySelectorAll('.color');
-let bookContent = document.querySelector('book__content');
+let bookContent = document.querySelector('.book__content');
 
 function deleteClassSize() {
 	let activeButton = document.querySelector('.font-size_active');
@@ -42,21 +42,21 @@ for (let textColor of textColors) {
 		if (textColor.getAttribute('data-text-color') == 'black') {
 			deleteClassColor();
 			textColor.classList.add('color_active');
-			bookContent.classList.add("text_color_black");
-			bookContent.classList.remove("text_color_gray");
-			bookContent.classList.remove("text_color_whitesmoke");
+			bookContent.classList.add("book_color-black");
+			bookContent.classList.remove("book_color-gray");
+			bookContent.classList.remove("book_color-whitesmoke");
 		} else if (textColor.getAttribute('data-text-color') == 'gray') {
 			deleteClassColor();
 			textColor.classList.add('color_active');
-			bookContent.classList.remove("text_color_black");
-			bookContent.classList.add("text_color_gray");
-			bookContent.classList.remove("text_color_whitesmoke");
+			bookContent.classList.remove("book_color-black");
+			bookContent.classList.add("book_color-gray");
+			bookContent.classList.remove("book_color-whitesmoke");
 		} else {
 			deleteClassColor();
 			textColor.classList.add('color_active');
-			bookContent.classList.remove("text_color_black");
-			bookContent.classList.remove("text_color_gray");
-			bookContent.classList.add("text_color_whitesmoke");
+			bookContent.classList.remove("book_color-black");
+			bookContent.classList.remove("book_color-gray");
+			bookContent.classList.add("book_color-whitesmoke");
 		}
 	});
 }
@@ -67,21 +67,21 @@ for (let bgColor of bgColors) {
 		if (bgColor.getAttribute('data-bg-color') == 'black') {
 			deleteClassColor();
 			bgColor.classList.add('color_active');
-			bookContent.classList.add("bg_color_black");
-			bookContent.classList.remove("bg_color_gray");
-			bookContent.classList.remove("bg_color_white");
+			bookContent.classList.add("book_bg-black");
+			bookContent.classList.remove("book_bg-gray");
+			bookContent.classList.remove("book_bg-white");
 		} else if (bgColor.getAttribute('data-bg-color') == 'gray') {
 			deleteClassColor();
 			bgColor.classList.add('color_active');
-			bookContent.classList.remove("bg_color_black");
-			bookContent.classList.add("bg_color_gray");
-			bookContent.classList.remove("bg_color_white");
+			bookContent.classList.remove("book_bg-black");
+			bookContent.classList.add("book_bg-gray");
+			bookContent.classList.remove("book_bg-white");
 		} else {
 			deleteClassColor();
 			bgColor.classList.add('color_active');
-			bookContent.classList.remove("bg_color_black");
-			bookContent.classList.remove("bg_color_gray");
-			bookContent.classList.add("bg_color_white");
+			bookContent.classList.remove("book_bg-black");
+			bookContent.classList.remove("book_bg-gray");
+			bookContent.classList.add("book_bg-white");
 		}
 	});
 }
