3bd384ae55782f087e42599271e2e2e8c98a84e1
Nikita Shalimov <nikitashalimov@mail.ru>
2021-10-18 21:53:25 +0300

    6_3 finnally

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,36 +1,47 @@
-const products = document.querySelectorAll('.product');
-const shoppingCart = document.querySelector('.cart__products');
+const products = document.querySelectorAll('.product'); // основной блок
+const shoppingCart = document.querySelector('.cart__products'); // корзина
 
 products.forEach(item => {
-	const lotMinus = item.querySelector('.product__quantity-control_dec');
-	const lot = item.querySelector('.product__quantity-value');
-	const lotPlus = item.querySelector('.product__quantity-control_inc');
-	const buy = item.querySelector('.product__add');
+	const lotMinus = item.querySelector('.product__quantity-control_dec'); // минус
+	const lot = item.querySelector('.product__quantity-value'); // кол-во 
+	const lotPlus = item.querySelector('.product__quantity-control_inc'); // плюс
+	const buy = item.querySelector('.product__add'); // кнопка купить
+	// увеличиваем кол-во товара
 	lotPlus.addEventListener('click', () => {
 		lot.innerHTML++;
 	})
+	// уменьшаем кол-во товара
 	lotMinus.addEventListener('click', () => {
 		lot.innerHTML--;
 		if (lot.innerHTML < 1) {
 			lot.innerHTML = 1;
 		}
 	})
+	// добавляем товар в корзину
 	buy.addEventListener('click', () => {
 		const image = item.querySelector('img');
 		const index = item.getAttribute('data-id');
 		const cartProducts = shoppingCart.querySelectorAll('.cart__product');
-		const product = cartProducts.find(i => {
+		//при покупке ищем такой же елемент в корзине
+		let product = Array.from(cartProducts).find(i => {
 			if (i.getAttribute('data-id') == index) {
-				let value = product.querySelector('.cart__product-count');
-				value.innerText = value.innerHTML + lot.innerHTML;
-				return;
+				return i;
 			}
-		})
-		shoppingCart.innerHTML += `
-		<div class="cart__product" data-id=${index}>
-                <img class="cart__product-image" src=${image.getAttribute('src')}>
-                <div class="cart__product-count">${lot.innerHTML}</div>
-            </div>
-		`;
+		});
+		//если элемент есть, добавляем кол-во товаров и выходим из функции покупки
+		if (product) {
+			let value = product.querySelector('.cart__product-count');
+			value.innerText = Number(value.innerHTML) + Number(lot.innerHTML);
+			lot.innerHTML = 1;
+			return;
+		}
+		// если в корзине такого элемента еще нет, то добавляем его
+		shoppingCart.insertAdjacentHTML('beforeend', `
+			<div class="cart__product" data-id=${index}>
+					<img class="cart__product-image" src=${image.getAttribute('src')}>
+					<div class="cart__product-count">${lot.innerHTML}</div>
+				</div>
+			`);
+		lot.innerHTML = 1;
 	})
 });
