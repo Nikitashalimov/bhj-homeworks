59f436f9e78f7f78faca9c44b1adb9e31e3c526b
Nikita Shalimov <nikitashalimov@mail.ru>
2021-11-18 20:29:04 +0300

    исправил

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -13,35 +13,9 @@ sendButton.addEventListener('click', (e) => {
 function upload() {
 	let formData = new FormData(form);
 	let xhr = new XMLHttpRequest();
-	xhr.open('POST', url);
-	xhr.send(formData);
 	// обработчик процесса
 	xhr.upload.onprogress = function (e) {
-		let percent = Math.floor(e.loaded / e.total * 100);
-		console.log(percent);
-		if (percent === 0) {
-			progress.value = 0.0;
-		} else if (percent === 10) {
-			progress.value = 0.1;
-		} else if (percent === 20) {
-			progress.value = 0.2;
-		} else if (percent === 30) {
-			progress.value = 0.3;
-		} else if (percent === 40) {
-			progress.value = 0.4;
-		} else if (percent === 50) {
-			progress.value = 0.5;
-		} else if (percent === 60) {
-			progress.value = 0.6;
-		} else if (percent === 70) {
-			progress.value = 0.7;
-		} else if (percent === 80) {
-			progress.value = 0.8;
-		} else if (percent === 90) {
-			progress.value = 0.9;
-		} else if (percent === 100) {
-			progress.value = 1.0;
-		}
+		progress.value = e.loaded / e.total;
 	}
 	// обработчик успеха и ошибки
 	xhr.onload = () => {
@@ -53,4 +27,6 @@ function upload() {
 			log("error " + this.status);
 		}
 	}
+	xhr.open('POST', url);
+	xhr.send(formData);
 }
